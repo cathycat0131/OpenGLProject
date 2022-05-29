b5fddd2b8b7de460fb2bbed3ce3340fee44d33ad
Cathy Chen <cathycat0131@gmail.com>
2022-05-28 20:07:30 -0700

    Initial Commit

---
+++ OpenGLProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGLProject
+//
+//  Created by Cathy Chen on 2022-05-28.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
