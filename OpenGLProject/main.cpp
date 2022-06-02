aaf103cd8ddff455986f321b9bcca686b3d99e6f
Cathy Chen <cathycat0131@gmail.com>
2022-06-02 12:26:35 -0700

    Added recorded video and picture

--- OpenGLProject/main.cpp
+++ OpenGLProject/main.cpp
@@ -83,7 +83,7 @@ int main( void )
     
     CreateObject();
     CreateShaders();
-    
+     
     camera = Camera(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), -90.0f, 0.0f, 1.0f, 0.1f);
     
     GLuint uniformProjection = 0, uniformModel = 0, uniformView = 0;
